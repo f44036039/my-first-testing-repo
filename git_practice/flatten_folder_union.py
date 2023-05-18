32789b04c90550c0d48ea2d1ae9a85452193278a
f44036039 <f44036039@gs.ncku.edu.tw>
2023-05-18 00:20:19 -0700

    Rreplace name with jedi and added create file structure function

--- git_practice/flatten_folder_union.py
+++ git_practice/flatten_folder_union.py
@@ -56,16 +56,15 @@ def create_list(folder_name):
         level_2_list = os.listdir(level_1_list[i])
 
         if '.DS_Store' in level_2_list:
-            level_2_list.remove('.DS_Store')
+            level_2_list.remove('.DS_Store') # for Mac
 
         for itr in level_2_list:
             set_of_level_2.add(itr)
 
         if folder_name in level_2_list:
-            jedi_list = os.listdir(os.path.join(level_1_list[i],folder_name))
-            jedi_dict = create_dict(jedi_list, i, level_1_list, folder_name)
-            total_list.append(jedi_dict)
-
+            file_list = os.listdir(os.path.join(level_1_list[i],folder_name))
+            file_dict = create_dict(file_list, i, level_1_list, folder_name)
+            total_list.append(file_dict)
 
     return total_list
 
@@ -98,15 +97,16 @@ def create_2d_list(result,dir_name):
         for itr in value:
             list1.append(itr)
         result_list.append(list1)
+
     '''wb = openpyxl.load_workbook('/Users/xuhaoxiang/code/empire.xlsx')
     ws = wb.active
     print(result)
-
     
         # print(list1)
 
     os.chdir('/Users/xuhaoxiang/code')
     wb.save('empire.xlsx')'''
+    
     return result_list
 
 
@@ -123,25 +123,27 @@ def main(root):
         
         return result_list
 
-# root = 'Trial'
-root = '/Users/xuhaoxiang/Documents/test_2'
+def create_file_structure(result_list, root_dir_name):
+
+    for i in range(len(result_list)):
+
+        info = result_list[i]
+
+        dir_name = info[0]
+
+        file_name = info[1]
+
+        content = info[2:]
+
+        if not os.path.exists(root_dir_name + '/' + dir_name):
+            os.makedirs(root_dir_name + '/' + dir_name)
+
+        with open(root_dir_name + '/' + dir_name + '/' + file_name, 'w') as f:
+            for line in content:
+                f.write(line)
+                f.write('\n')
+
+# root = 'Trial' # for ASUS
+root = '/Users/xuhaoxiang/Documents/test_2' # for Mac
 result_list = main(root)
-
-for i in range(len(result_list)):
-
-    info = result_list[i]
-
-    dir_name = info[0]
-
-    file_name = info[1]
-
-    content = info[2:]
-
-    root_dir_name = 'empireDirectory'
-    if not os.path.exists(root_dir_name + '/' + dir_name):
-        os.makedirs(root_dir_name + '/' + dir_name)
-
-    with open(root_dir_name + '/' + dir_name + '/' + file_name, 'w') as f:
-        for line in content:
-            f.write(line)
-            f.write('\n')
+create_file_structure(result_list, 'empireDirectory')
