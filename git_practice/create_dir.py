a9f99a8e53b870b2a7feb8c1e89eee33d909d632
f44036039 <126354285+f44036039@users.noreply.github.com>
2023-05-25 10:47:54 -0700

    Update create_dir.py with an if statement

--- git_practice/create_dir.py
+++ git_practice/create_dir.py
@@ -1,24 +1,35 @@
 import os
 import pandas as pd
 
-data = pd.read_excel('empire.xlsx')
+create_folder_structure_flag = False
+print("please put the excel file under this directory: ", os.getcwd())
+excel_file_name = 'file_structure.xlsx'
+data = pd.read_excel(excel_file_name)
 print(data)
 
-for i in range(len(data)):
+if create_folder_structure_flag == True:
 
-    info = data.iloc[i,:].dropna()
+    for i in range(len(data)):
 
-    dir_name = info[0]
+        info = data.iloc[i,:].dropna()
 
-    file_name = info[1]
+        dir_name = info[0]
 
-    content = info[2:]
+        file_name = info[1]
 
-    root_dir_name = 'empireDirectory'
-    if not os.path.exists(root_dir_name + '/' + dir_name):
-        os.makedirs(root_dir_name + '/' + dir_name)
+        content = info[2:]
 
-    with open(root_dir_name + '/' + dir_name + '/' + file_name, 'w') as f:
-        for line in content:
-            f.write(line)
-            f.write('\n')
+        root_dir_name = 'empireDirectory_excel'
+        dir_path = os.path.join(root_dir_name, dir_name) # root_dir_name + '/' + dir_name
+
+        if not os.path.exists(dir_path): 
+            os.makedirs(dir_path)
+
+        file_path = os.path.join(dir_path, file_name)
+
+        with open(file_path, 'w') as f:
+            for line in content:
+                f.write(line)
+                f.write('\n')
+else:
+    print('Did not create folder structure from excel')
